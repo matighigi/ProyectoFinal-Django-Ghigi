eb57162534fd5358d2e42dc0c96b80b8d62caa7c
matighigi <matiasghigi@gmail.com>
2025-12-26 01:18:00 -0300

    login & logout funcionales

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 urlpatterns = [
+    # Signup (registro)
     path("signup/", SignUpView.as_view(), name="signup"),
+
+    # Login / Logout (CBV de Django)
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
@@ -16,6 +16,17 @@ class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        """
+        Si el formulario es válido:
+        - crea el usuario
+        - muestra un mensaje de éxito
+        - y redirige al login
+        """
+        response = super().form_valid(form)
+        messages.success(self.request, "Cuenta creada. Ahora podés iniciar sesión.")
+        return response
 
 """
 CBV para registrar usuarios.

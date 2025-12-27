d81641b934823c62ad0c20afefb7d93fac61a9be
matighigi <matiasghigi@gmail.com>
2025-12-27 15:43:23 -0300

    funciones pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import PageListView, PageDetailView
+from .views import PageListView, PageDetailView, PageCreateView, PageUpdateView, PageDeleteView
 
 urlpatterns = [
     # /pages/ (paginas)
     path("", PageListView.as_view(), name="pages_list"),
 
+    # /pages/create/ (creación paginas)
+    path("create/", PageCreateView.as_view(), name="pages_create"),
+
     # /pages/1/  (detalle de paginas)
     path("<int:pk>/", PageDetailView.as_view(), name="pages_detail"),
+
+    # /pages/1/update/ (actualización paginas)
+    path("<int:pk>/update/", PageUpdateView.as_view(), name="pages_update"),
+
+    # /pages/1/delete/ (eliminación paginas)
+    path("<int:pk>/delete/", PageDeleteView.as_view(), name="pages_delete"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Page
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #Lista de páginas: /pages/
 class PageListView(ListView):
@@ -13,3 +15,33 @@ class PageDetailView(DetailView):
     model = Page
     template_name = "pages/page_detail.html"
     context_object_name = "page"
+
+# Crear una nueva página: /pages/new/
+class PageCreateView(LoginRequiredMixin, CreateView):
+    """
+    - LoginRequiredMixin: obliga a estar logueado
+    - CreateView: Django se encarga del formulario y guardado
+    """
+    model = Page
+    template_name = "pages/page_form.html"
+    fields = ["title", "subtitle", "content", "image"]
+    success_url = reverse_lazy("pages_list")
+    login_url = reverse_lazy("login")
+
+# Editar una página existente: /pages/<id>/edit/
+class PageUpdateView(LoginRequiredMixin, UpdateView):
+    model = Page
+    template_name = "pages/page_form.html"
+    fields = ["title", "subtitle", "content", "image"]
+    success_url = reverse_lazy("pages_list")
+    login_url = reverse_lazy("login")
+
+# Eliminar una página: /pages/<id>/delete/
+class PageDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    Muestra una pantalla de confirmación.
+    """
+    model = Page
+    template_name = "pages/page_confirm_delete.html"
+    success_url = reverse_lazy("pages_list")
+    login_url = reverse_lazy("login")
